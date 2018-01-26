e15209eab7bf2d1a4c532cb4c957c355eb286bc5
David Perrenoud <david.perrenoud@ti8m.ch>
2018-01-26 17:06:58 +0100

    Add levels to SimpleCertifier

--- SimpleCertifier.sol
+++ SimpleCertifier.sol
@@ -20,23 +20,30 @@ import "Owned.sol";
 import "Certifier.sol";
 
 contract SimpleCertifier is Owned, Certifier {
+	enum Level {
+		Revoked,
+		Level1,
+		Level2,
+		Level3
+	}
+
 	modifier only_delegate { if (msg.sender != delegate) return; _; }
-	modifier only_certified(address _who) { if (!certs[_who].active) return; _; }
+	modifier only_certified(address _who) { if (certs[_who].activeLevel == Level.Revoked) return; _; }
 
 	struct Certification {
-		bool active;
+		Level activeLevel;
 		mapping (string => bytes32) meta;
 	}
 
-	function certify(address _who) only_delegate {
-		certs[_who].active = true;
+	function certify(address _who, uint level) only_delegate {
+		certs[_who].activeLevel = Level(level);
 		Confirmed(_who);
 	}
 	function revoke(address _who) only_delegate only_certified(_who) {
-		certs[_who].active = false;
+		certs[_who].activeLevel = Level.Revoked;
 		Revoked(_who);
 	}
-	function certified(address _who) constant returns (bool) { return certs[_who].active; }
+	function certified(address _who) constant returns (bool) { return (certs[_who].activeLevel != Level.Revoked); }
 	function get(address _who, string _field) constant returns (bytes32) { return certs[_who].meta[_field]; }
 	function getAddress(address _who, string _field) constant returns (address) { return address(certs[_who].meta[_field]); }
 	function getUint(address _who, string _field) constant returns (uint) { return uint(certs[_who].meta[_field]); }
