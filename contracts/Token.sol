9b075ce1fad597f2d188e5cd6b2a66d34d847c71
Andrea Magazzini <anz@ti8m.ch>
2018-01-30 17:03:11 +0100

    removed interface

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.2;
 
-interface Token {
+contract Token {
 	event Transfer(address indexed from, address indexed to, uint256 value);
 	event Approval(address indexed owner, address indexed spender, uint256 value);
 
