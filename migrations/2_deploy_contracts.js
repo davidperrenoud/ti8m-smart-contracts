aaadb7b1b3466502c54aa7a177cfd14a25a4fe25
Andrea Magazzini <anz@ti8m.ch>
2018-01-30 14:13:04 +0100

    fix for deploz

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,15 +2,14 @@ let SimpleCertifier = artifacts.require('./SimpleCertifier.sol');
 let DutchAuction = artifacts.require('./DutchAuction.sol');
 let BasicCoin = artifacts.require('./BasicCoin.sol');
 
-const _treasury = ' 0xf17f52151ebef6c7334fad080c5704d77216b732';
-const _admin = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
-//const _beginTime = Math.round(+new Date()/1000);
-const _beginTime = 1517303425;
-const _beginPrice = 10000;
-const _saleSpeed = 1;
-const _tokenCap = 10;
+module.exports = function (deployer, network, accounts) {
+    const _treasury = accounts[1];
+    const _admin = accounts[0];
+    const _beginTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp
+    const _beginPrice = 10000;
+    const _saleSpeed = 1;
+    const _tokenCap = 10;
 
-module.exports = function (deployer) {
     deployer.deploy(SimpleCertifier).then(() => {
         return deployer.deploy(BasicCoin, 10, _admin).then(() => {
             return deployer.deploy(DutchAuction, SimpleCertifier.address, BasicCoin.address, _treasury, _admin,
