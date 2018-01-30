32ba9cf9c7c7221105a747d31729d2ea568a0590
Andrea Magazzini <anz@ti8m.ch>
2018-01-30 16:04:19 +0100

    changed comments

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,3 +1,4 @@
+/*
 //! The ECR20 standard-token interface.
 //!
 //! Copyright 2016 Peter Czaban, Parity Technologies Ltd.
@@ -13,6 +14,7 @@
 //! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //! See the License for the specific language governing permissions and
 //! limitations under the License.
+*/
 
 pragma solidity ^0.4.2;
 
