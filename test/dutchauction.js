18972136e6d8af146508ad772dc33fda8703e27b
Andrea Magazzini <anz@ti8m.ch>
2018-01-30 16:44:55 +0100

    try to change the function call

--- test/dutchauction.js
+++ test/dutchauction.js
@@ -1,24 +1,24 @@
 var DutchAuction = artifacts.require('./contracts/DutchAuction.sol');
 var SimpleCertifier = artifacts.require('./contracts/SimpleCertifier.sol');
 
-contract('DutchAuction', (accounts) => {
-    it('should check if the auction is active', () => {
-        return DutchAuction.deployed().then((instance) => {
+contract('DutchAuction', function(accounts){
+    it('should check if the auction is active', function(){
+        return DutchAuction.deployed().then(function(instance){
             return instance.isActive();
         }).then((response) => {
             assert.equal(response, true, 'the auction is not active');
         });
     });
-    it('should check if the account is certified', () => {
-        return SimpleCertifier.deployed().then((instance) => {
+    it('should check if the account is certified', function(){
+        return SimpleCertifier.deployed().then(function(instance) {
             instance.certify(accounts[0], 1);
             return instance.certified(accounts[0]);
         }).then(function (response) {
             assert.equal(response, true, 'the account is not certified');
         });
     });
-    it('should check if a certified account can buy', () => {
-        return DutchAuction.deployed().then((instance) => {
+    it('should check if a certified account can buy', function() {
+        return DutchAuction.deployed().then(function(instance) {
 
             let amount = web3.toWei(0.01, "ether");
 
@@ -30,8 +30,8 @@ contract('DutchAuction', (accounts) => {
             assert.ok(response.tx, 'transaction not working');
         });
     });
-    it('should check if the account is not certified', () => {
-        return SimpleCertifier.deployed().then((instance) => {
+    it('should check if the account is not certified', function() {
+        return SimpleCertifier.deployed().then(function(instance) {
             instance.revoke(accounts[0]);
             return instance.certified(accounts[0]);
         }).then(function (response) {
@@ -39,7 +39,7 @@ contract('DutchAuction', (accounts) => {
         });
     });
     it('should check if a not certified account can buy', () => {
-        return DutchAuction.deployed().then((instance) => {
+        return DutchAuction.deployed().then(function(instance) {
 
             let amount = web3.toWei(0.01, "ether");
 
