33f897b04460c63f3a59f00122739e8b5aa4a361
Andrea Magazzini <anz@ti8m.ch>
2018-01-30 17:29:06 +0100

    fix truffle.js

--- truffle.js
+++ truffle.js
@@ -1,3 +1,11 @@
 module.exports = {
-
+    rpc: {
+        host: "localhost",
+        port: 8545
+    },
+    networks:{
+        "production": {
+            "network_id": 20170123
+        }
+    }
 };
