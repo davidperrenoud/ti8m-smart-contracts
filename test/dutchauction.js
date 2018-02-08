d683c9d527ed76fc18b649e2ecddeed8bb2cddf2
David Perrenoud <david.perrenoud@ti8m.ch>
2018-02-08 19:12:11 +0100

    Reformat tests

--- test/dutchauction.js
+++ test/dutchauction.js
@@ -1,80 +1,85 @@
-var DutchAuction = artifacts.require('./contracts/DutchAuction.sol');
-var SimpleCertifier = artifacts.require('./contracts/SimpleCertifier.sol');
+var DutchAuction = artifacts.require("./contracts/DutchAuction.sol");
+var SimpleCertifier = artifacts.require("./contracts/SimpleCertifier.sol");
 
-contract('DutchAuction', function(accounts){
-    it('should check if the auction is active', function(){
-        return DutchAuction.deployed().then(function(instance){
-            return instance.isActive();
-        }).then((response) => {
-            assert.equal(response, true, 'the auction is not active');
-        });
-    });
-    it('should check if a certified levels account can buy according to the limit', function() {
-        return SimpleCertifier.deployed().then(function(instance) {
-            return instance.certify(accounts[1], 1, 0, 0).then(function(){
-                return DutchAuction.deployed().then(function(instance) {
-
-                    let amount = web3.toWei(0.5, "ether");
-
-                    return instance.sendTransaction({
-                                                        from: accounts[1],
-                                                        value: amount
-                                                    });
-                }).then(function (response) {
-                    assert.ok(response.tx, 'transaction not working');
-                });
+contract("DutchAuction", function(accounts) {
+  it("should check if the auction is active", () => {
+    return DutchAuction.deployed()
+      .then((instance) => {
+        return instance.isActive();
+      })
+      .then(response => {
+        assert.equal(response, true, "the auction is not active");
+      });
+  });
+  it("should check if a certified levels account can buy according to the limit", () => {
+    return SimpleCertifier.deployed().then(instance => {
+      return instance.certify(accounts[1], 1, 0, 0).then(() => {
+        return DutchAuction.deployed()
+          .then(instance => {
+            const amount = web3.toWei(0.5, "ether");
+            return instance.sendTransaction({
+              from: accounts[1],
+              value: amount
             });
+          })
+          .then(response => {
+            assert.ok(response.tx, "transaction not working");
+          });
+      });
 
-            return instance.certify(accounts[2], 2, 0, 0).then(function(){
-                return DutchAuction.deployed().then(function(instance) {
-
-                    let amount = web3.toWei(2, "ether");
-                    return instance.sendTransaction({
-                                                        from: accounts[2],
-                                                        value: amount
-                                                    });
-                }).then(function (response) {
-                    assert.ok(response.tx, 'transaction not working');
-                });
+      return instance.certify(accounts[2], 2, 0, 0).then(() => {
+        return DutchAuction.deployed()
+          .then(instance => {
+            const amount = web3.toWei(2, "ether");
+            return instance.sendTransaction({
+              from: accounts[2],
+              value: amount
             });
-        })
+          })
+          .then(response => {
+            assert.ok(response.tx, "transaction not working");
+          });
+      });
     });
-    it('should check if a not certified account can buy', function() {
-        return SimpleCertifier.deployed().then(function(instance) {
-            return instance.revoke(accounts[0]).then(function(){
-                return DutchAuction.deployed().then(function(instance) {
-
-                    let amount = web3.toWei(1, "ether");
-                    return instance.sendTransaction({
-                                                        from: accounts[0],
-                                                        value: amount
-                                                    });
-                }).then(function(response) {
-                    assert(false, "it was supposed to throw an error but didn't.");
-                }).catch(function(error) {
-                    assert(true, "Test failed.");
-                });
+  });
+  it("should check if a not certified account can buy", () => {
+    return SimpleCertifier.deployed().then((instance) => {
+      return instance.revoke(accounts[0]).then(() => {
+        return DutchAuction.deployed()
+          .then((instance) => {
+            const amount = web3.toWei(1, "ether");
+            return instance.sendTransaction({
+              from: accounts[0],
+              value: amount
             });
-        })
+          })
+          .then((response) => {
+            assert(false, "it was supposed to throw an error but didn't.");
+          })
+          .catch((error) => {
+            assert(true, "the test failed.");
+          });
+      });
     });
-    it('should check if a certified level 1 account can buy with a higher limit', function() {
-        return SimpleCertifier.deployed().then(function(instance) {
-            return instance.certify(accounts[1], 1, 0, 0).then(function(){
-                return DutchAuction.deployed().then(function(instance) {
-
-                    let amount = web3.toWei(2, "ether");
-                    return instance.sendTransaction({
-                                                        from: accounts[2],
-                                                        value: amount
-                                                    });
-                }).then(function(response) {
-                    assert(false, "it was supposed to throw an error but didn't.");
-                }).catch(function(error) {
-                    assert(true, "Test failed.");
-                });
+  });
+  it("should check if a certified level 1 account can buy with a higher limit", () => {
+    return SimpleCertifier.deployed().then((instance) => {
+      return instance.certify(accounts[1], 1, 0, 0).then(() => {
+        return DutchAuction.deployed()
+          .then((instance) => {
+            const amount = web3.toWei(2, "ether");
+            return instance.sendTransaction({
+              from: accounts[2],
+              value: amount
             });
-        })
+          })
+          .then((response) => {
+            assert(false, "it was supposed to throw an error but didn't.");
+          })
+          .catch((error) => {
+            assert(true, "the test failed.");
+          });
+      });
     });
+  });
 });
-
-
--- test/simplecertifier.js
+++ test/simplecertifier.js
@@ -1,32 +1,44 @@
 var SimpleCertifier = artifacts.require("./contracts/SimpleCertifier.sol");
 
-contract('SimpleCertifier', function(accounts) {
-    it('should certify an account for level 1', function(){
-        return SimpleCertifier.deployed().then(function(instance) {
-            return instance.certify(accounts[0], 1, 0, 0).then(function(){
-                return instance.certified(accounts[0]);
-            });
-        }).then(function (response) {
-            assert.equal(response, true, 'the account is not certified for level 2');
+contract("SimpleCertifier", function(accounts) {
+  it("should certify an account for level 1", () => {
+    return SimpleCertifier.deployed()
+      .then(function(instance) {
+        return instance.certify(accounts[0], 1, 0, 0).then(function() {
+          return instance.certified(accounts[0]);
         });
-    });
-    it("should certify an account for level 2", function() {
-        return SimpleCertifier.deployed().then(function(instance) {
-            return instance.certify(accounts[0], 2, 0, 0).then(function(){
-                return instance.highlyCertified(accounts[0]);
-            });
-        }).then(function (response) {
-            assert.equal(response, true, 'the account is not certified for level 2');
+      })
+      .then(function(response) {
+        assert.equal(
+          response,
+          true,
+          "the account is not certified for level 1"
+        );
+      });
+  });
+  it("should certify an account for level 2", () => {
+    return SimpleCertifier.deployed()
+      .then(function(instance) {
+        return instance.certify(accounts[0], 2, 0, 0).then(function() {
+          return instance.highlyCertified(accounts[0]);
         });
-    });
-    it("should revoke the certification for an account", function() {
-        return SimpleCertifier.deployed().then(function(instance) {
-            instance.revoke(accounts[0]);
-            return instance.certified(accounts[0]);
-        }).then(function(response) {
-            assert.equal(response, false, "the account is certified");
-        });
-    });
+      })
+      .then(function(response) {
+        assert.equal(
+          response,
+          true,
+          "the account is not certified for level 2"
+        );
+      });
+  });
+  it("should revoke the certification for an account", () => {
+    return SimpleCertifier.deployed()
+      .then(function(instance) {
+        instance.revoke(accounts[0]);
+        return instance.certified(accounts[0]);
+      })
+      .then(function(response) {
+        assert.equal(response, false, "the account is certified");
+      });
+  });
 });
-
-
