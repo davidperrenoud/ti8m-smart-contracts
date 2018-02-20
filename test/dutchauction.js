9c57bbde8fadc64bac6aee2840236f988e58c595
David Perrenoud <david.perrenoud@ti8m.ch>
2018-02-20 16:21:48 +0100

    Prettier

--- test/dutchauction.js
+++ test/dutchauction.js
@@ -4,7 +4,7 @@ var SimpleCertifier = artifacts.require("./contracts/SimpleCertifier.sol");
 contract("DutchAuction", function(accounts) {
   it("should check if the auction is active", () => {
     return DutchAuction.deployed()
-      .then((instance) => {
+      .then(instance => {
         return instance.isActive();
       })
       .then(response => {
@@ -43,40 +43,40 @@ contract("DutchAuction", function(accounts) {
     });
   });
   it("should check if a not certified account can buy", () => {
-    return SimpleCertifier.deployed().then((instance) => {
+    return SimpleCertifier.deployed().then(instance => {
       return instance.revoke(accounts[0]).then(() => {
         return DutchAuction.deployed()
-          .then((instance) => {
+          .then(instance => {
             const amount = web3.toWei(1, "ether");
             return instance.sendTransaction({
               from: accounts[0],
               value: amount
             });
           })
-          .then((response) => {
+          .then(response => {
             assert(false, "it was supposed to throw an error but didn't.");
           })
-          .catch((error) => {
+          .catch(error => {
             assert(true, "the test failed.");
           });
       });
     });
   });
   it("should check if a certified level 1 account can buy with a higher limit", () => {
-    return SimpleCertifier.deployed().then((instance) => {
+    return SimpleCertifier.deployed().then(instance => {
       return instance.certify(accounts[1], 1, 0, 0).then(() => {
         return DutchAuction.deployed()
-          .then((instance) => {
+          .then(instance => {
             const amount = web3.toWei(2, "ether");
             return instance.sendTransaction({
               from: accounts[2],
               value: amount
             });
           })
-          .then((response) => {
+          .then(response => {
             assert(false, "it was supposed to throw an error but didn't.");
           })
-          .catch((error) => {
+          .catch(error => {
             assert(true, "the test failed.");
           });
       });
