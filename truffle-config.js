d06b002e0796f389f9d8b5bdd4f16621c9738a63
Cino Ruggero <ruggero.cino@ti8m.ch>
2018-03-14 11:48:24 +0100

    Rename truffle in truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,11 @@
 const ganache = require("ganache-cli");
 
 module.exports = {
-  rpc: {
-    host: "localhost",
-    port: 8545
-  },
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 4712388
     },
-    in_memory: {
-      provider: ganache.provider(),
-      network_id: "*"
-    }
   }
 };
