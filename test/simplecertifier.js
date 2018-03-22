bcb2af859b432c91c83c27042f6c7ebeff74badb
Cino Ruggero <ruggero.cino@ti8m.ch>
2018-03-22 11:59:59 +0100

    Add test on onlyDelegate modifier, code clean

--- test/simplecertifier.js
+++ test/simplecertifier.js
@@ -1,59 +1,70 @@
 var SimpleCertifier = artifacts.require("./contracts/SimpleCertifier.sol");
 
-contract("SimpleCertifier", function(accounts) {
-  it("should certify an account for level 1", () => {
-    return SimpleCertifier.deployed()
-      .then(function(instance) {
-        return instance.certify(accounts[0], 1, accounts[0]).then(function() {
-          return instance.certified(accounts[0]);
-        });
+contract("SimpleCertifier", (accounts) => {
+  administrator = accounts[5];
+  simpleCertifier = null;
+
+  beforeEach((done) => {
+    SimpleCertifier.new({ from: administrator })
+      .then(contract => {
+        simpleCertifier = contract;
+        done();
       })
-      .then(function(response) {
-        assert.equal(
-          response,
-          1,
-          "the account is not certified for level 1"
-        );
-      });
+      .catch(error => done(error));
+  })
+
+  it("should not certify an account if not delegate", (done) => {
+    simpleCertifier.certify(accounts[0], 1, accounts[0], { from: accounts[1] })
+      .then(() => simpleCertifier.certified(accounts[0]))
+      .then((response) => {
+        assert.equal(response, 0, "account certified for level 1");
+        done();
+      })
+      .catch(error => done(error));
+  })
+
+  it("should certify an account for level 1", (done) => {
+    simpleCertifier.certify(accounts[0], 1, accounts[0], { from: administrator })
+      .then(() => simpleCertifier.certified(accounts[0]))
+      .then((response) => {
+        assert.equal(response, 1, "account not certified for level 1");
+        done();
+      })
+      .catch(error => done(error));
   });
-  it("should certify an account for level 2", () => {
-    return SimpleCertifier.deployed()
-      .then(function(instance) {
-        return instance.certify(accounts[0], 2, accounts[0]).then(function() {
-          return instance.certified(accounts[0]);
-        });
+
+  it("should certify an account for level 2", (done) => {
+    simpleCertifier.certify(accounts[0], 2, accounts[0], { from: administrator })
+      .then(() => simpleCertifier.certified(accounts[0]))
+      .then((response) => {
+        assert.equal(response, 2, "account not certified for level 2");
+        done();
       })
-      .then(function(response) {
-        assert.equal(
-          response,
-          2,
-          "the account is not certified for level 2"
-        );
-      });
+      .catch(error => done(error));
   });
-  it("should certify an account for level 3", () => {
-    return SimpleCertifier.deployed()
-      .then(function(instance) {
-        return instance.certify(accounts[0], 3, accounts[0]).then(function() {
-          return instance.certified(accounts[0]);
-        });
+
+  it("should certify an account for level 3", (done) => {
+    simpleCertifier.certify(accounts[0], 3, accounts[0], { from: administrator })
+      .then(() => simpleCertifier.certified(accounts[0]))
+      .then((response) => {
+        assert.equal(response, 3, "account not certified for level 3");
+        done();
       })
-      .then(function(response) {
-        assert.equal(
-          response,
-          3,
-          "the account is not certified for level 3"
-        );
-      });
+      .catch(error => done(error));
   });
-  it("should revoke the certification for an account", () => {
-    return SimpleCertifier.deployed()
-      .then(function(instance) {
-        instance.revoke(accounts[0]);
-        return instance.certified(accounts[0]);
+
+  it("should revoke the certification for an account", (done) => {
+    simpleCertifier.certify(accounts[0], 3, accounts[0], { from: administrator })
+      .then(() => simpleCertifier.certified(accounts[0]))
+      .then((response) => {
+        assert.equal(response, 3, "account not certified for level 3");
+        return simpleCertifier.revoke(accounts[0], { from: administrator });
       })
-      .then(function(response) {
+      .then(() => simpleCertifier.certified(accounts[0]))
+      .then((response) => {
         assert.equal(response, 0, "the account is certified");
-      });
+        done();
+      })
+      .catch(error => done(error));
   });
 });
